1bcee79e6b1a1999f8f3427cc170f0d8dfdd7f48
vidit <viditsharma818@gmail.com>
2023-08-23 23:02:39 +0530

    logout funtinality

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,11 +1,29 @@
+"use client"
+import axios from 'axios'
 import React from 'react'
-
+import { toast } from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
 function page() {
+  const router=useRouter()
+  async function handleLogout(){
+    
+      try {
+        await axios.get("/api/users/logout")
+        toast.success("logout successful")
+        router.push("/")
+      } catch (error) {
+        console.log(error.message)
+        toast.error(error.message)
+      }
+    
+
+  }
   return (
     <div>
         <h1>Profile page</h1>
         <h1>hii Your name</h1>
-        
+        <hr />
+        <button onClick={handleLogout}>Logout</button>
         </div>
   )
 }
